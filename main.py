dbdd320484a61e9cb45f3d69a396e32c299694e5
dmorrison <dan.r.morrison@gmail.com>
2024-10-29 09:18:13 -0400

    Edits to main.py

--- main.py
+++ main.py
@@ -9,4 +9,21 @@ def wordcount():
     count = len(words)
     return count
 
-print(wordcount())
+def characters():
+    book = main()
+    chars = {}
+    for letter in book:
+        chars[letter.lower()] = int(0)
+    for letter in book:
+        chars[letter.lower()] += 1
+    return(chars)
+
+def report():
+    chars = characters()
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{wordcount()} words found in the document\n")
+    for i in chars:
+        print(f"The {i} character was found {chars[i]} times")
+    print("--- End report ---")
+
+report()
